16816913c015cb03ba1caaedb88e71a075c1edb3
rocky <rocky@memoio.io>
2024-11-27 13:57:57 +0800

    add nvcc12.sh

---
+++ nvcc12.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/bin:$PATH
+export CPATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/include:$CPATH
+export LD_LIBRARY_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/lib64:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/lib:$DYLD_LIBRARY_PATH
